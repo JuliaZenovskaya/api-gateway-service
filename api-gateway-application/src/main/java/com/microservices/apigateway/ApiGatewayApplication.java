46b78f014ae9d2870378b096045456ad74652465
JuliaZenovskaya <juliazenovskaya@gmail.com>
2019-11-29 01:35:59 +0300

    zuul

--- api-gateway-application/src/main/java/com/microservices/apigateway/ApiGatewayApplication.java
+++ api-gateway-application/src/main/java/com/microservices/apigateway/ApiGatewayApplication.java
@@ -1,10 +1,12 @@
 package com.microservices.apigateway;
 
+import com.microservices.apigateway.filters.ApiGatewayFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -13,9 +15,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableFeignClients
 @EnableSwagger2
 public class ApiGatewayApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
+    @Bean
+    public ApiGatewayFilter apiGatewayFilter(){
+        return new ApiGatewayFilter();
+    }
 }
--- api-gateway-application/src/main/java/com/microservices/apigateway/filters/ApiGatewayFilter.java
+++ api-gateway-application/src/main/java/com/microservices/apigateway/filters/ApiGatewayFilter.java
@@ -1,4 +1,31 @@
 package com.microservices.apigateway.filters;
 
-public class ApiGatewayFilter {
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class ApiGatewayFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest httpServletRequest = requestContext.getRequest();
+        return null;
+    }
 }
